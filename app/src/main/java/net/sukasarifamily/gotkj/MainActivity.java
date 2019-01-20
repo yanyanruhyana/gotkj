0d778a2a5d08ffa2d43023742c3883f8e9b9c8a4
Mas Taka <yanyanruhyana@gmail.com>
2019-01-21 06:25:46 +0700

    tambah java

--- app/src/main/java/net/sukasarifamily/gotkj/MainActivity.java
+++ app/src/main/java/net/sukasarifamily/gotkj/MainActivity.java
@@ -2,12 +2,49 @@ package net.sukasarifamily.gotkj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtjarak, txtharga, txttip;
+    TextView tvhasil;
+    Button btntunai, btnvoucher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtjarak =findViewById(R.id.txtjarak);
+        txtharga = findViewById(R.id.txtharga);
+        txttip = findViewById(R.id.txttip);
+        tvhasil = findViewById(R.id.tvhasil);
+        btntunai = findViewById(R.id.btntunai);
+        btnvoucher = findViewById(R.id.btnvoucher);
+
+        btntunai.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int j = Integer.parseInt(txtjarak.getText().toString());
+                int h = Integer.parseInt(txtharga.getText().toString());
+                int t = Integer.parseInt(txttip.getText().toString());
+                int jumlah = (j * h) + t;
+                tvhasil.setText(String.valueOf(jumlah));
+            }
+        });
+
+        btnvoucher.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int j = Integer.parseInt(txtjarak.getText().toString());
+                int h = Integer.parseInt(txtharga.getText().toString());
+                int t = Integer.parseInt(txttip.getText().toString());
+                int jumlah = ((j * h)*10/100) + t;
+                tvhasil.setText(String.valueOf(jumlah));
+            }
+        });
+
     }
 }
